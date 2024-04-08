ef68db33e9c554a78da0ebc164e6392eda64606e
jianjiuchen <chenjianjiu@gmail.com>
2024-04-08 07:30:36 -0400

    change to allow scraping for multiple news outlets

--- scrape_news.py
+++ scrape_news.py
@@ -1,57 +1,34 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-
-import time
 from datetime import datetime
 import pytz
-import sqlite3
+
+from utils import scrape_from_single_outlet, store_for_single_outlet
+
 
 proj_dir = '/Users/Chen/project/sentiment_analysis/'
 today = datetime.now(pytz.timezone('Asia/Hong_Kong')).date()
+news_outlets = (('SCMP', 'https://www.scmp.com/topics/hong-kong-stock-exchange'),
+                )
 
 print('******************************')
 print('Log for', today)
 print('******************************')
 
-driver = webdriver.Chrome()
+for outlet_name, outlet_url in news_outlets:
+    print('Start for ', outlet_name, '.', sep='')
 
-driver.get("https://www.scmp.com/news/hong-kong/hong-kong-economy")
-
-# Scroll down to trigger the loading of additional content
-# Adjust the number of scrolls and the sleep time as needed
-for _ in range(10):
-    ActionChains(driver).scroll_by_amount(delta_x=0,delta_y=2000).perform()
-    time.sleep(5)
-
-html_content = driver.page_source
-
-driver.quit()
-
-soup = BeautifulSoup(html_content, 'html.parser')
-
-titles = []
-h2s = soup.find_all('h2')
-for i, h2_i in enumerate(h2s):
     try:
-        titles.append(h2_i.text)
-    except AttributeError as err:
+        titles = scrape_from_single_outlet(outlet_name, outlet_url)
+    except Exception as err:
+        print('Error occurred when scraping from ', outlet_name, '!', sep='')
         print(err)
+        continue
 
-headlines_today = [(str(today),i.replace('\n','').strip()) for i in titles]
+    try:
+        store_for_single_outlet(outlet_name, titles, today, proj_dir)
+    except Exception as err:
+        print('Error occurred when storing for ', outlet_name, '!', sep='')
+        print(err)
+        continue
 
-con = sqlite3.connect(proj_dir + "news.db")
-cur = con.cursor()
-
-# need to create a new table on the first day
-news_outlet_names = cur.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
-news_outlet_names = [i[0] for i in news_outlet_names]
-if 'SCMP' not in news_outlet_names:
-    cur.execute("CREATE TABLE SCMP(date, headline)")
-
-cur.executemany("INSERT INTO SCMP VALUES(?, ?)", headlines_today)
-con.commit()
-con.close()
-
-print('SCMP: ')
-print(titles, end='\n\n')
+    print('End scraping and storing for ', outlet_name, '. Titles stored were:', sep='')
+    print(titles, end='\n\n')
